2aa6bbe28497abb8df217b8b6597aa6700f16ba1
duanecorrea <vinicius.correa18@gmail.com>
2020-09-21 19:24:42 -0300

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-      path: 'rosearch',
+      path: 'search',
       loadChildren: () => import('./ragapp/ragapp.module').then(m => m.RagAppModule)
   }
 ];
